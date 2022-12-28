3ade14b61a84e5f7e65fd679c04a27a718083191
Ethan Netz <ethannetz@hotmail.com>
2022-12-27 23:30:55 -0800

    welcome 2 seattle

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div>Testing deployment</div>
+          <div>Hello violet beb</div>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page</Link>
           <div>
